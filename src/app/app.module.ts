f4793be48440e3b4f10b94fc9dad7f56dabf11df
LAPTOP-MI62FDND\B.ROSSUKON <phatcharaphonp@pccth.com>
2022-08-03 15:38:35 +0700

    first commit angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetDataComponent } from './component/get-data/get-data.component';
+import { ShowDataComponent } from './component/show-data/show-data.component';
+import { MatIconModule } from '@angular/material/icon'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetDataComponent,
+    ShowDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
